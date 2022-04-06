06697f8b9ac364e6605987a5778c45dc339485f1
Aris Vayas <aris.vayas@gmail.com>
2022-04-06 16:41:34 -0400

    commit tree 86b4b4b14b0fe42bddbeba07e49ec83309155194

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+
+function findMatching(names,str) {
+    
+return  names.filter(function(item){
+return item.toUpperCase() == str.toUpperCase()
+})
+    
+}
+
+const fuzzyMatch = (driver,str) =>{
+ 
+ return driver.filter(function(driver){
+     
+     return driver.substring(0,str.length)==str
+     
+ }
+ )}
+ function matchName(driver,string){
+     return driver.filter(function(driver,hometown){
+         return driver.name == string
+     }
+
+     )}
